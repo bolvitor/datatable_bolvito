ac0cfb859000dc0e528dbcacdced47067aa0d83d
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-08-23 11:14:46 -0600

    actualizacion de repositorio datatable

--- public/index.php
+++ public/index.php
@@ -4,10 +4,18 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\AppController;
+use Controllers\LoginController;
+use Controllers\ProductoController;
+
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
-$router->get('/', [AppController::class,'index']);
+$router->get('/', [LoginController::class,'index']);
+$router->get('/menu', [LoginController::class,'menu']);
+$router->get('/logout', [LoginController::class,'logout']);
+$router->post('/API/login', [LoginController::class,'loginAPI']);
+$router->get('/productos/datatable', [ProductoController::class,'datatable']);
+$router->get('/API/productos/buscar', [ProductoController::class,'buscarAPI']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
---
+++ views/menu/index.php
@@ -0,0 +1,3 @@
+<h1>menu principal</h1>
+<a href="/datatable_bolvito/productos/datatable" class="btn btn-link">Datatable</a>
+<a href="/datatable_bolvito/logout" class="btn btn-danger">Cerrar sesión</a>
